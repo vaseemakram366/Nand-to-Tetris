8e1a7e94caf40a20564bb5057832e155ccbc32fa
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-09 21:38:21 +0530

    Learned number system

--- Week1/day2.py
+++ Week1/day2.py
@@ -1,224 +1,224 @@
 
 
-# def decimal_to_binary(num):
-#     if num == 0:
-#         return "0"
+def decimal_to_binary(num):
+    if num == 0:
+        return "0"
 
-#     binary = ""
+    binary = ""
 
-#     while num > 0:
-#         remainder = num % 2
-#         binary = str(remainder) + binary
-#         num = num // 2
+    while num > 0:
+        remainder = num % 2
+        binary = str(remainder) + binary
+        num = num // 2
 
-#     return binary
+    return binary
 
 
-# # Driver code
-# number = 22
-# print("Decimal:", number)
-# print("Binary:", decimal_to_binary(number))
+# Driver code
+number = 22
+print("Decimal:", number)
+print("Binary:", decimal_to_binary(number))
 
 
-# def binary_to_decimal(binary):
-#     decimal = 0
-#     power = 0
+def binary_to_decimal(binary):
+    decimal = 0
+    power = 0
 
-#     # Traverse from right to left
-#     for digit in binary[::-1]:
-#         if digit == '1':
-#             decimal += 2 ** power
-#         power += 1
+    # Traverse from right to left
+    for digit in binary[::-1]:
+        if digit == '1':
+            decimal += 2 ** power
+        power += 1
 
-#     return decimal
+    return decimal
 
 
-# # Driver code
-# binary = "1010"
-# print("Binary:", binary)
-# print("Decimal:", binary_to_decimal(binary))
+# Driver code
+binary = "1010"
+print("Binary:", binary)
+print("Decimal:", binary_to_decimal(binary))
 
 
-# # DECIMAL TO OCTAL
-# def decimal_to_octal(num):
-#     if num == 0:
-#         return "0"
+# DECIMAL TO OCTAL
+def decimal_to_octal(num):
+    if num == 0:
+        return "0"
 
-#     octal = ""
+    octal = ""
 
-#     while num > 0:
-#         remainder = num % 8
-#         octal = str(remainder) + octal
-#         num = num // 8
+    while num > 0:
+        remainder = num % 8
+        octal = str(remainder) + octal
+        num = num // 8
 
-#     return octal
+    return octal
 
 
-# # Driver code
-# number = 123
-# print("Decimal:", number)
-# print("Octal:", decimal_to_octal(number))
+# Driver code
+number = 123
+print("Decimal:", number)
+print("Octal:", decimal_to_octal(number))
 
-# # OCTAL TO DECIMAL
-# def octal_to_decimal(octal):
-#     decimal = 0
-#     power = 0
+# OCTAL TO DECIMAL
+def octal_to_decimal(octal):
+    decimal = 0
+    power = 0
 
-#     # Traverse from right to left
-#     for digit in octal[::-1]:
-#         decimal += int(digit) * (8 ** power)
-#         power += 1
+    # Traverse from right to left
+    for digit in octal[::-1]:
+        decimal += int(digit) * (8 ** power)
+        power += 1
 
-#     return decimal
+    return decimal
 
 
-# # Driver code
-# octal = "173"
-# print("Octal:", octal)
-# print("Decimal:", octal_to_decimal(octal))
+# Driver code
+octal = "173"
+print("Octal:", octal)
+print("Decimal:", octal_to_decimal(octal))
 
-# # DECIMAL TO HEXADECIMAL
-# def decimal_to_hex(num):
-#     if num == 0:
-#         return "0"
+# DECIMAL TO HEXADECIMAL
+def decimal_to_hex(num):
+    if num == 0:
+        return "0"
 
-#     hex_digits = "0123456789ABCDEF"
-#     hexa = ""
+    hex_digits = "0123456789ABCDEF"
+    hexa = ""
 
-#     while num > 0:
-#         remainder = num % 16
-#         hexa = hex_digits[remainder] + hexa
-#         num = num // 16
+    while num > 0:
+        remainder = num % 16
+        hexa = hex_digits[remainder] + hexa
+        num = num // 16
 
-#     return hexa
+    return hexa
 
 
-# # Driver code
-# number = 123
-# print("Decimal:", number)
-# print("Hexadecimal:", decimal_to_hex(number))
+# Driver code
+number = 123
+print("Decimal:", number)
+print("Hexadecimal:", decimal_to_hex(number))
 
 
-# BINARY TO OCTAL
+BINARY TO OCTAL
 
-# def binary_to_octal(binary):
-#     decimal = 0
+def binary_to_octal(binary):
+    decimal = 0
 
-#     # Binary to Decimal
-#     power = 0
-#     for digit in binary[::-1]:
-#         decimal += int(digit) * (2 ** power)
-#         power += 1
+    # Binary to Decimal
+    power = 0
+    for digit in binary[::-1]:
+        decimal += int(digit) * (2 ** power)
+        power += 1
 
-#     # Decimal to Octal
-#     if decimal == 0:
-#         return "0"
+    # Decimal to Octal
+    if decimal == 0:
+        return "0"
 
-#     octal = ""
-#     while decimal > 0:
-#         octal = str(decimal % 8) + octal
-#         decimal //= 8
+    octal = ""
+    while decimal > 0:
+        octal = str(decimal % 8) + octal
+        decimal //= 8
 
-#     return octal
+    return octal
 
 
-# # Driver code
-# binary = "1010"
-# print("Binary:", binary)
-# print("Octal:", binary_to_octal(binary))
+# Driver code
+binary = "1010"
+print("Binary:", binary)
+print("Octal:", binary_to_octal(binary))
 
-# OCTAL TO BINARY
+OCTAL TO BINARY
 
-# def octal_to_binary(octal):
-#     # Step 1: Octal to Decimal
-#     decimal = 0
-#     power = 0
+def octal_to_binary(octal):
+    # Step 1: Octal to Decimal
+    decimal = 0
+    power = 0
 
-#     for digit in octal[::-1]:
-#         decimal += int(digit) * (8 ** power)
-#         power += 1
+    for digit in octal[::-1]:
+        decimal += int(digit) * (8 ** power)
+        power += 1
 
-#     # Step 2: Decimal to Binary
-#     if decimal == 0:
-#         return "0"
+    # Step 2: Decimal to Binary
+    if decimal == 0:
+        return "0"
 
-#     binary = ""
-#     while decimal > 0:
-#         binary = str(decimal % 2) + binary
-#         decimal //= 2
+    binary = ""
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
 
-#     return binary
+    return binary
 
 
-# # Driver code
-# octal = "24"
-# print("Octal:", octal)
-# print("Binary:", octal_to_binary(octal))
+# Driver code
+octal = "24"
+print("Octal:", octal)
+print("Binary:", octal_to_binary(octal))
 
 
-# BINARY TO HEXADECIMAL
+BINARY TO HEXADECIMAL
 
-# def binary_to_hex(binary):
-#     # Step 1: Binary to Decimal
-#     decimal = 0
-#     power = 0
+def binary_to_hex(binary):
+    # Step 1: Binary to Decimal
+    decimal = 0
+    power = 0
 
-#     for digit in binary[::-1]:
-#         decimal += int(digit) * (2 ** power)
-#         power += 1
+    for digit in binary[::-1]:
+        decimal += int(digit) * (2 ** power)
+        power += 1
 
-#     # Step 2: Decimal to Hexadecimal
-#     hex_digits = "0123456789ABCDEF"
+    # Step 2: Decimal to Hexadecimal
+    hex_digits = "0123456789ABCDEF"
 
-#     if decimal == 0:
-#         return "0"
+    if decimal == 0:
+        return "0"
 
-#     hexa = ""
-#     while decimal > 0:
-#         hexa = hex_digits[decimal % 16] + hexa
-#         decimal //= 16
+    hexa = ""
+    while decimal > 0:
+        hexa = hex_digits[decimal % 16] + hexa
+        decimal //= 16
 
-#     return hexa
+    return hexa
 
 
-# # Driver code
-# binary = "1010"
-# print("Binary:", binary)
-# print("Hexadecimal:", binary_to_hex(binary))
+# Driver code
+binary = "1010"
+print("Binary:", binary)
+print("Hexadecimal:", binary_to_hex(binary))
 
 
-# OCTAL TO HEXADECIMAL
+OCTAL TO HEXADECIMAL
 
-# def octal_to_hex(octal):
-#     # Step 1: Octal to Decimal
-#     decimal = 0
-#     power = 0
+def octal_to_hex(octal):
+    # Step 1: Octal to Decimal
+    decimal = 0
+    power = 0
 
-#     for digit in octal[::-1]:
-#         decimal += int(digit) * (8 ** power)
-#         power += 1
+    for digit in octal[::-1]:
+        decimal += int(digit) * (8 ** power)
+        power += 1
 
-#     # Step 2: Decimal to Hexadecimal
-#     hex_digits = "0123456789ABCDEF"
+    # Step 2: Decimal to Hexadecimal
+    hex_digits = "0123456789ABCDEF"
 
-#     if decimal == 0:
-#         return "0"
+    if decimal == 0:
+        return "0"
 
-#     hexa = ""
-#     while decimal > 0:
-#         hexa = hex_digits[decimal % 16] + hexa
-#         decimal //= 16
+    hexa = ""
+    while decimal > 0:
+        hexa = hex_digits[decimal % 16] + hexa
+        decimal //= 16
 
-#     return hexa
+    return hexa
 
 
-# # Driver code
-# octal = "247"
-# print("Octal:", octal)
-# print("Hexadecimal:", octal_to_hex(octal))
+# Driver code
+octal = "247"
+print("Octal:", octal)
+print("Hexadecimal:", octal_to_hex(octal))
 
 
-# HEXADECIMAL TO OCTAL
+HEXADECIMAL TO OCTAL
 
 def hex_to_octal(hexa):
     hex_digits = "0123456789ABCDEF"
